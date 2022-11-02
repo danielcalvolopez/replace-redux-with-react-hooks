fe971a937352f42b08ab69fdff8fd078857286a3
Daniel <acidistrict@gmail.com>
2022-11-02 17:55:00 +0000

    implementing context and replace redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { combineReducers, createStore } from "redux";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import ProductsContextProvider from "./context/products-context";
 import "./index.css";
 import App from "./App";
-import productReducer from "./store/reducers/products";
-
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
-
-const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <ProductsContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProductsContextProvider>
 );
