66327c08f877c45ddeb51420ecdcaecffdebb696
Daniel <acidistrict@gmail.com>
2022-11-03 22:47:57 +0000

    destructuring props

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -4,23 +4,23 @@ import Card from "../UI/Card";
 import "./ProductItem.css";
 import { ProductsContext } from "../../context/products-context";
 
-const ProductItem = (props) => {
+const ProductItem = ({ id, isFav, title, description }) => {
   const toggleFav = useContext(ProductsContext).toggleFav;
 
   const toggleFavHandler = () => {
-    toggleFav(props.id);
+    toggleFav(id);
   };
 
   return (
     <Card style={{ marginBottom: "1rem" }}>
       <div className="product-item">
-        <h2 className={props.isFav ? "is-fav" : ""}>{props.title}</h2>
-        <p>{props.description}</p>
+        <h2 className={isFav ? "is-fav" : ""}>{title}</h2>
+        <p>{description}</p>
         <button
-          className={!props.isFav ? "button-outline" : ""}
+          className={!isFav ? "button-outline" : ""}
           onClick={toggleFavHandler}
         >
-          {props.isFav ? "Un-Favorite" : "Favorite"}
+          {isFav ? "Un-Favorite" : "Favorite"}
         </button>
       </div>
     </Card>
