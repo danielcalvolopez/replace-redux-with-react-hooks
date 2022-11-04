c60a75974a02f3820b0ccd85a302783af4b805a7
Daniel <acidistrict@gmail.com>
2022-11-04 16:51:10 +0000

    custom hook and store added and redux replaced succesfully

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -1,46 +1,46 @@
-import React, { createContext, useState } from "react";
+import React, { useState } from 'react';
 
-export const ProductsContext = createContext({
+export const ProductsContext = React.createContext({
   products: [],
-  toggleFav: (id) => {},
+  toggleFav: (id) => {}
 });
 
-const ProductsContextProvider = ({ children }) => {
+export default props => {
   const [productsList, setProductsList] = useState([
     {
-      id: "p1",
-      title: "Red Scarf",
-      description: "A pretty red scarf.",
-      isFavorite: false,
+      id: 'p1',
+      title: 'Red Scarf',
+      description: 'A pretty red scarf.',
+      isFavorite: false
     },
     {
-      id: "p2",
-      title: "Blue T-Shirt",
-      description: "A pretty blue t-shirt.",
-      isFavorite: false,
+      id: 'p2',
+      title: 'Blue T-Shirt',
+      description: 'A pretty blue t-shirt.',
+      isFavorite: false
     },
     {
-      id: "p3",
-      title: "Green Trousers",
-      description: "A pair of lightly green trousers.",
-      isFavorite: false,
+      id: 'p3',
+      title: 'Green Trousers',
+      description: 'A pair of lightly green trousers.',
+      isFavorite: false
     },
     {
-      id: "p4",
-      title: "Orange Hat",
-      description: "Street style! An orange hat.",
-      isFavorite: false,
-    },
+      id: 'p4',
+      title: 'Orange Hat',
+      description: 'Street style! An orange hat.',
+      isFavorite: false
+    }
   ]);
 
-  const toggleFavorite = (productId) => {
-    setProductsList((currentProdList) => {
-      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+  const toggleFavorite = productId => {
+    setProductsList(currentProdList => {
+      const prodIndex = currentProdList.findIndex(p => p.id === productId);
       const newFavStatus = !currentProdList[prodIndex].isFavorite;
       const updatedProducts = [...currentProdList];
       updatedProducts[prodIndex] = {
         ...currentProdList[prodIndex],
-        isFavorite: newFavStatus,
+        isFavorite: newFavStatus
       };
       return updatedProducts;
     });
@@ -50,9 +50,7 @@ const ProductsContextProvider = ({ children }) => {
     <ProductsContext.Provider
       value={{ products: productsList, toggleFav: toggleFavorite }}
     >
-      {children}
+      {props.children}
     </ProductsContext.Provider>
   );
 };
-
-export default ProductsContextProvider;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import ProductsContextProvider from "./context/products-context";
+
 import "./index.css";
 import App from "./App";
+import configureProductsStore from "./hooks-store/products-store";
+
+configureProductsStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ProductsContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsContextProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
